e176931f524bf60bf55f40a6821f1e3bfd61ab03
IkarandashI <v.vitaminkin@gmail.com>
2024-02-21 19:53:34 +0300

    Create and destroy zombie weapon in c++

--- Source/ZombiSurvival/Private/AI/SurvZombiCharacter.cpp
+++ Source/ZombiSurvival/Private/AI/SurvZombiCharacter.cpp
@@ -4,6 +4,8 @@
 #include "AI/SurvZombiCharacter.h"
 #include "Components/PlayerStatsComponent.h"
 #include "Components/CapsuleComponent.h"
+#include "Components/TraceComponent.h"
+#include "Weapon/BaseMeleeWeapon.h"
 
 // Sets default values
 ASurvZombiCharacter::ASurvZombiCharacter(const class FObjectInitializer& ObjectInitializer) 
@@ -20,11 +22,56 @@ ASurvZombiCharacter::ASurvZombiCharacter(const class FObjectInitializer& ObjectI
 
 }
 
+void ASurvZombiCharacter::CreateLeftWeapon()
+{
+	FTransform SocketTransform = GetMesh()->GetSocketTransform(TEXT("LeftWeaponSocket"));
+	if (IsValid(ZombieWeaponClass))
+	{
+		LeftMeleeWeaponref = GetWorld()->SpawnActor<ABaseMeleeWeapon>(ZombieWeaponClass, SocketTransform);
+		if (LeftMeleeWeaponref)
+		{
+			LeftMeleeWeaponref->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, TEXT("LeftWeaponSocket"));
+			LeftMeleeWeaponref->GetTraceComponent()->MeleeWeapon->SetOwner(this);
+		}
+	}
+}
+
+void ASurvZombiCharacter::CreateRightWeapon()
+{
+	FTransform SocketTransform = GetMesh()->GetSocketTransform(TEXT("RightWeaponSocket"));
+	if (IsValid(ZombieWeaponClass))
+	{
+		RightMeleeWeaponref = GetWorld()->SpawnActor<ABaseMeleeWeapon>(ZombieWeaponClass, SocketTransform);
+		if (RightMeleeWeaponref)
+		{
+			RightMeleeWeaponref->AttachToComponent(GetMesh(), FAttachmentTransformRules::SnapToTargetIncludingScale, TEXT("RightWeaponSocket"));
+			RightMeleeWeaponref->GetTraceComponent()->MeleeWeapon->SetOwner(this);
+		}
+	}
+}
+
+void ASurvZombiCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CreateLeftWeapon();
+	CreateRightWeapon();
+}
+
 float ASurvZombiCharacter::TakeDamage(float DamageAmount, FDamageEvent const& DamageEvent, AController* EventInstigator,
-	AActor* DamageCauser)
+                                      AActor* DamageCauser)
 {
 	SetTargetActor();
 	return Super::TakeDamage(DamageAmount, DamageEvent, EventInstigator, DamageCauser);
+}
+
+void ASurvZombiCharacter::OnDeath(float KillingDamage, FDamageEvent const& DamageEvent, APawn* PawnInstigator,
+	AActor* DamageCauser)
+{
+	Super::OnDeath(KillingDamage, DamageEvent, PawnInstigator, DamageCauser);
+	RightMeleeWeaponref->Destroy();
+	LeftMeleeWeaponref->Destroy();
+	StopAnimMontage();
 }
 
 void ASurvZombiCharacter::SetTargetActor_Implementation()
